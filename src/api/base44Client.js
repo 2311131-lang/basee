219bdc80144c1d20f2fe3b86bde259cddf441707
2311131-lang <2311131@dlu.edu.vn>
2026-05-26 10:54:18 +0700

    Update base44Client.js

--- src/api/base44Client.js
+++ src/api/base44Client.js
@@ -2,4 +2,7 @@ import { createClient } from "@base44/sdk";
 
 export const base44 = createClient({
   appId: "6a1071561cb625e244b350d6",
+  headers: {
+    "api_key": "a2664a871748490bad3bb48ff99e85f0"
+  }
 });
